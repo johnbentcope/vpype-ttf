2610c0a5220db9ed312d4d7fa2f3d7cf40784779
Tatarize <tatarize@gmail.com>
2023-01-03 21:36:35 -0800

    initial working version

--- vpype_ttf/font.py
+++ vpype_ttf/font.py
@@ -11,11 +11,15 @@ import freetype
 @vpype_cli.global_processor
 def render(document: vp.Document, filename: str, text: str):
     face = freetype.Face(filename)
+    resolution_factor = 1024
+    font_size = 16 # 16px = 12pt
 
     # Set the font size in pixels
-    face.set_char_size(48 * 64)
+    face.set_char_size(resolution_factor * 96)
 
     pen = freetype.Vector()
+    x_position = 0
+    y_position = 0
     for character in text:
         lc = vp.LineCollection()
         face.load_char(character)
@@ -23,12 +27,16 @@ def render(document: vp.Document, filename: str, text: str):
         outline = slot.outline
         start, end = 0, 0
         for contour in range(len(outline.contours)):
+            # print(f"New Contour: {contour}")
             verts = []
             end = outline.contours[contour]
             points = outline.points[start:end + 1]
             tags = outline.tags[start:end + 1]
+            points.append(points[0])
+            tags.append(tags[0])
+            start = end + 1
             segments = [[points[0], ], ]
-            for j in range(1, len(points)):
+            for j in range(0, len(points)):
                 segments[-1].append(points[j])
                 if tags[j] & 1:
                     segments.append([points[j], ])
@@ -61,18 +69,23 @@ def render(document: vp.Document, filename: str, text: str):
                     xs = n_pos_3 * x0 + 3 * (n_pos_2_pos * x1 + pos_2_n_pos * x2) + pos_3 * x3
                     ys = n_pos_3 * y0 + 3 * (n_pos_2_pos * y1 + pos_2_n_pos * y2) + pos_3 * y3
                     verts.extend(xs + 1j * ys)
-                else:
+                elif len(segment) == 2:
                     x, y = segment[-1]
                     verts.append(x + 1j * y)
-
             verts = np.array(verts, dtype="complex")
+            verts += x_position + 1j * y_position
             lc.append(verts)
-        pen.x += slot.advance.x
-        pen.y += slot.advance.y
+        dx = slot.advance.x
+        dy = slot.advance.y
+        x_position += dx
+        y_position += dy
+        pen.x += dx
+        pen.y += dy
+        lc.scale(font_size/resolution_factor,-font_size/resolution_factor)
         document.add(lc)
     del pen
     del face
     return document
 
 
-render.help_group = "Font"
+render.help_group = "Text"
