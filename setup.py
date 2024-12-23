c64c7720d3d924aaad95050e20557a006e733421
John Cope <johnbentcope@gmail.com>
2024-12-23 17:29:11 -0500

    v0.0.6 time

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="vpype-ttf",
-    version="0.0.5",
+    version="0.0.6",
     description="vpype ttf plugin",
     long_description=readme,
     long_description_content_type="text/markdown",
