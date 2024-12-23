e57858f2ed3bc4e02f719cd078504def605ef554
John Cope <johnbentcope@gmail.com>
2024-12-23 17:24:07 -0500

    Incrementing to v0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="vpype-ttf",
-    version="0.0.4",
+    version="0.0.5",
     description="vpype ttf plugin",
     long_description=readme,
     long_description_content_type="text/markdown",
